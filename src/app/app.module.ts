1fd6a443ccdeb2ea0e8738f08739fc1b6cc5409d
Tim Kirves <tim.kirves@gmail.com>
2020-05-14 03:31:44 +0300

    components update, party.service update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './Components/header/header.component';
-import { FooterComponent } from './Components/footer/footer.component';
-import { CreatePartyComponent } from './CRUD/create-party/create-party.component';
-import { BodyComponent } from './Components/body/body.component';
-import { PartyDetailsComponent } from './CRUD/party-details/party-details.component';
-import { PartyListComponent } from './CRUD/party-list/party-list.component';
-import { UpdatePartyComponent } from './CRUD/update-party/update-party.component';
-
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { AppComponent } from 'src/app/app.component';
+import { HeaderComponent } from 'src/app/Components/header/header.component';
+import { FooterComponent } from 'src/app/Components/footer/footer.component';
+import { CreatePartyComponent } from 'src/app/CRUD/create-party/create-party.component';
+import { BodyComponent } from 'src/app/Components/body/body.component';
+import { PartyDetailsComponent } from 'src/app/CRUD/party-details/party-details.component';
+import { PartyListComponent } from 'src/app/CRUD/party-list/party-list.component';
+import { UpdatePartyComponent } from 'src/app/CRUD/update-party/update-party.component';
 @NgModule({
   declarations: [
     AppComponent,
